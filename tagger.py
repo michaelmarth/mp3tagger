ae8c717c007d817c177658cf6bfa8bffb4fa9002
Michael Marth <michael.marth@gmail.com>
2010-01-08 17:37:54 +0100

    first commit

---
+++ tagger.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+tag_groupings.py
+
+Created by Michael Marth on 2009-11-02.
+Copyright (c) 2010 marth.software.services. All rights reserved.
+"""
+
+import sys
+import getopt
+import pylast
+import os.path
+import ConfigParser
+from mutagen.id3 import TCON, ID3, TIT1
+
+help_message = '''
+Adds ID3 tags to mp3 files for genre and groupings. Tag values are retrieved from Last.FM. Usage:
+-d mp3_directory
+'''
+
+class Usage(Exception):
+	def __init__(self, msg):
+		self.msg = msg
+
+all_genres = TCON.GENRES
+genre_cache = {}
+groupings_cache = {}
+
+
+def artist_to_genre(artist):
+	if genre_cache.has_key(artist):
+		return genre_cache[artist]
+	else:
+		tags = network.get_artist(artist).get_top_tags()		
+		for tag in tags:
+			if all_genres.__contains__(tag[0].name.title()):
+				genre_cache[artist] = tag[0].name.title()
+				print "%20s %s" % (artist,tag[0].name.title())
+				return tag[0].name.title()
+
+def artist_to_groupings(artist):
+	if groupings_cache.has_key(artist):
+		return groupings_cache[artist]
+	else:
+		tags = network.get_artist(artist).get_top_tags()
+		relevant_tags = []
+		for tag in tags:
+			if int(tag[1]) >= 50:
+				relevant_tags.append(tag[0].name.title())
+		groupings = ", ".join(relevant_tags)
+		groupings_cache[artist] = groupings
+		print "%20s %s" % (artist,groupings)
+		return groupings
+
+def walk_mp3s():
+	for root, dirs, files in os.walk('.'):
+		for name in files:
+			if name.endswith(".mp3"):
+				audio = ID3(os.path.join(root, name))
+				artist = audio["TPE1"]
+				genre = artist_to_genre(artist[0])
+				grouping = artist_to_groupings(artist[0])
+				if genre != None:
+					audio["TCON"] = TCON(encoding=3, text=genre)
+				if grouping != None:
+					audio["TIT1"] = TIT1(encoding=3, text=grouping)
+				audio.save()
+
+def setup_lastfm():
+	config = ConfigParser.ConfigParser()
+	config.read(os.path.expanduser('~/.mp3tagger.cfg'))
+	if config.has_option("last.fm", "key") and config.has_option("last.fm", "secret"):
+		API_KEY = config.get("last.fm", "key")
+		API_SECRET = config.get("last.fm", "secret")
+	else:
+		new_config = ConfigParser.SafeConfigParser()
+		new_config.add_section("last.fm")
+		new_config.set("last.fm", "key", raw_input("Enter your Last.fm key:"))
+		new_config.set("last.fm", "secret", raw_input("Enter your Last.fm secret:"))
+		API_KEY = new_config.get("last.fm", "key")
+		API_SECRET = new_config.get("last.fm", "secret")
+		print "NOTE: key and secret will be stored in ~/.mp3tagger.cfg"
+		with open('.mp3tagger.cfg', 'w') as configfile:
+		    new_config.write(configfile)
+	global network
+	network = pylast.get_lastfm_network(api_key = API_KEY, api_secret = API_SECRET)
+
+def main(argv=None):
+	if argv is None:
+		argv = sys.argv
+	try:
+		try:
+			opts, args = getopt.getopt(argv[1:], "ho:vd:", ["help", "output="])
+		except getopt.error, msg:
+			raise Usage(msg)
+		# setup last.fm network
+		setup_lastfm()
+		
+		# option processing
+		for option, value in opts:
+			if option == "-v":
+				verbose = True
+			if option in ("-h", "--help"):
+				raise Usage(help_message)
+			if option in ("-o", "--output"):
+				output = value
+			if option in ("-d"):
+				try:
+					os.chdir(value)
+				except Exception,e:
+					print "error with directory " + value
+					print e
+		walk_mp3s()		
+	
+	except Usage, err:
+		print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
+		print >> sys.stderr, "\t for help use --help"
+		return 2
+
+if __name__ == "__main__":
+	sys.exit(main())
