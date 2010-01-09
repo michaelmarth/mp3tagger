e049289e019264f8e570703706a028adbe0abbe4
Michael Marth <michael.marth@gmail.com>
2010-01-09 15:41:34 +0100

    refinements of genres

--- tagger.py
+++ tagger.py
@@ -31,6 +31,8 @@ RUN_MODE_NORMAL = 0
 RUN_MODE_SIMULATION = 1
 RUN_MODE_ASK = 2
 
+TAG_MODE_NORMAL = 0
+TAG_MODE_REFINE = 1
 
 def artist_to_genre(artist):
 	if genre_cache.has_key(artist):
@@ -57,25 +59,53 @@ def artist_to_groupings(artist):
 		print "%20s %s" % (artist,groupings)
 		return groupings
 
+def refine_genre(possible_refinements):
+	#print "looking for refinements for %s in %s" % (possible_refinements, refinement_genre_refinements)
+	for genre in possible_refinements:
+		if genre in refinement_genre_refinements:
+			return genre
+	return ""
+
+def select_audio(audio):
+	if tag_mode == TAG_MODE_NORMAL:
+		return True
+	elif audio.has_key("TCON"):
+		if audio["TCON"].text[0] == refinement_genre:
+			return True
+		else:
+			return False
+	else:
+		return False
+
 def walk_mp3s():
 	for root, dirs, files in os.walk('.'):
 		for name in files:
 			if name.endswith(".mp3"):
 				audio = ID3(os.path.join(root, name))
-				artist = audio["TPE1"]
-				genre = artist_to_genre(artist[0])
-				grouping = artist_to_groupings(artist[0])
-				if genre != None:
-					audio["TCON"] = TCON(encoding=3, text=genre)
-				if grouping != None:
-					audio["TIT1"] = TIT1(encoding=3, text=grouping)
+				if not select_audio(audio):
+					continue
+				if tag_mode == TAG_MODE_NORMAL:
+					artist = audio["TPE1"]
+					genre = artist_to_genre(artist[0])
+					grouping = artist_to_groupings(artist[0])
+					if genre != None:
+						audio["TCON"] = TCON(encoding=3, text=genre)
+					if grouping != None:
+						audio["TIT1"] = TIT1(encoding=3, text=grouping)
+				else:
+					if audio.has_key("TIT1"):
+						genre = refine_genre(audio["TIT1"].text[0].split(","))
+						if genre != "":
+							print "refining genre for artist %s from %s to %s" % (audio["TPE1"].text[0], audio["TCON"].text[0], genre)
+							audio["TCON"] = TCON(encoding=3, text=genre)
 				
 				# what shall we do with the file?
 				if run_mode == RUN_MODE_NORMAL:
-					print "saving file %s" % (name)
+					#print "saving file %s" % (name)
 					audio.save()
 				if run_mode == RUN_MODE_SIMULATION:
-					print "not saving file %s (simulation mode)" % (name)
+					#print "not saving file %s (simulation mode)" % (name)
+					pass
 				if run_mode == RUN_MODE_ASK:
 					yesno = raw_input("Save for file " + name + " (y/n) [y]")
 					if yesno == "y" or yesno == "":
@@ -83,14 +113,23 @@ def walk_mp3s():
 
 def setup_genres():
 	global all_genres
+	global refinement_genre_refinements	
 	all_genres = TCON.GENRES
 	config = ConfigParser.ConfigParser()
-	config.read(os.path.expanduser('~/mp3tagger_genres.cfg'))
+	config.read(os.path.expanduser('~/.mp3tagger_genres.cfg'))
 	if config.has_option("generic", "genres"):
 		all_genres.extend(config.get("generic", "genres").split(","))
+	if refinement_genre != "":
+		if config.has_section("refinements"):
+			if config.has_option("refinements", refinement_genre):
+				refinement_genre_refinements = config.get("refinements", refinement_genre).split(",")
+			else:
+				print "WARNING: you are running in refinement mode, but there are no refinements for genre %s defined" % (refinement_genre)
+		else:
+			print "WARNING: you are running in refinement mode, but there are no refinements defined"
 
 def setup_lastfm():
-	global last_fm_network	
+	global last_fm_network
 	config = ConfigParser.ConfigParser()
 	config.read(os.path.expanduser('~/.mp3tagger.cfg'))
 	if config.has_option("last.fm", "key") and config.has_option("last.fm", "secret"):
@@ -109,18 +148,24 @@ def setup_lastfm():
 	last_fm_network = pylast.get_lastfm_network(api_key = API_KEY, api_secret = API_SECRET)
 
 def main(argv=None):
+		
 	global run_mode
+	global tag_mode
+	global refinement_genre
+
 	run_mode = RUN_MODE_NORMAL
+	tag_mode = TAG_MODE_NORMAL
+	refinement_genre = ""
+	
 	if argv is None:
 		argv = sys.argv
 	try:
 		try:
-			opts, args = getopt.getopt(argv[1:], "hvd:m:", ["help"])
+			opts, args = getopt.getopt(argv[1:], "hvd:m:r:", ["help"])
 		except getopt.error, msg:
 			raise Usage(msg)
 		# setup last.fm network
 		setup_lastfm()
-		setup_genres()
 		
 		# option processing
 		for option, value in opts:
@@ -131,14 +176,20 @@ def main(argv=None):
 			if option in ("-m"):
 				if value == "simulation":
 					run_mode = RUN_MODE_SIMULATION
+					print "Running in simulation mode - nothing will be saved"
 				if value == "ask":
 					run_mode = RUN_MODE_ASK
+			if option in ("-r"):
+				refinement_genre = value
+				tag_mode = TAG_MODE_REFINE
+				print "refining genre %s" % (refinement_genre)
 			if option in ("-d"):
 				try:
 					os.chdir(value)
 				except Exception,e:
 					print "error with directory " + value
 					print e
+		setup_genres()
 		walk_mp3s()		
 	
 	except Usage, err:
